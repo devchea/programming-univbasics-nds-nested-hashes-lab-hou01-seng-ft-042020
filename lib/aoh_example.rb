72f27e3a5e74f2a6605ccfb33347680a303219e0
Richard Chea <rchea817@gmail.com>
2020-03-26 15:56:24 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -31,13 +31,6 @@ def literal_aoh
 end
 
 def aoh_lookup(aoh, row, key)
-  new_array = [
-    DON_G,
-    JOELLE_VD,
-    PAT_M,
-    KATE_G,
-    BRUCE_G
-  ]
   new_array = [:aoh][:row][:key]
   new_array
 end
